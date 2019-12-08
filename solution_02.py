9f8e4652292fa045ee95b93dd6ced0c876f94695
Mike Pagel <mike@mpagel.de>
2019-12-08 16:27:04 +0100

    tests working

---
+++ solution_02.py
@@ -0,0 +1,33 @@
+import enum
+import operator
+import typing as t
+
+
+@enum.unique
+class Opcode(enum.IntEnum):
+    EXIT = 99
+    ADD = 1
+    MULTIPLY = 2
+
+
+OPERATIONS = {
+    Opcode.ADD.value: operator.add,
+    Opcode.MULTIPLY.value: operator.mul,
+}
+
+
+def run(ram: t.List[int]) -> t.List[int]:
+    ip = 0
+
+    while (opcode := ram[ip]) != Opcode.EXIT:
+        try:
+            operation = OPERATIONS[opcode]
+        except ValueError:
+            print(f'Encountered unknown opcode {opcode} at position {ip}.')
+            break
+
+        ram[ram[ip + 3]] = operation(ram[ram[ip + 1]], ram[ram[ip + 2]])
+        ip += 4
+
+    print(f'Program state after execution: {ram!r}')
+    return ram
