63a71b7be545dd5fd8692a588534d46467a2bbf9
Mike Pagel <mike@mpagel.de>
2019-12-08 15:46:53 +0100

    simplified

--- solution_01.py
+++ solution_01.py
@@ -24,11 +24,8 @@ def compute_total_fuel_iterative(mass: int) -> int:
 def main():
     masses = pathlib.Path('input_01.txt').read_text().splitlines(keepends=False)
 
-    fule_reqs = (compute_fuel(int(m)) for m in masses)
-    print('Fuel without fuel mass', sum(fule_reqs))
-
-    fule_reqs = (compute_total_fuel(int(m)) for m in masses)
-    print('Fuel with fuel mass', sum(fule_reqs))
+    print('Fuel without fuel mass', sum(compute_fuel(int(m)) for m in masses))
+    print('Fuel with fuel mass', sum(compute_total_fuel(int(m)) for m in masses))
 
 
 if __name__ == '__main__':
