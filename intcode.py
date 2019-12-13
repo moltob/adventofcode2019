9ccc404fe9a65b5a99ed874a3e9815ecc1073c4b
Mike Pagel <mike@mpagel.de>
2019-12-14 00:59:21 +0100

    added disassembler trace

--- intcode.py
+++ intcode.py
@@ -1,4 +1,5 @@
 import enum
+import io
 import logging
 import pathlib
 import typing as t
@@ -25,22 +26,40 @@ class ParameterMode(enum.IntEnum):
     IMMEDIATE = 1
 
 
+class TraceItem:
+    def __init__(self, address):
+        self.address = address
+        self.instruction = None
+        self.mnemonic = None
+        self.arguments = []
+        self.result = None
+
+    def __str__(self):
+        elements = [f'{self.address:05}', self.mnemonic]
+        elements.extend(self.arguments)
+        if self.result:
+            elements.append(f'-->')
+            elements.append(self.result)
+
+        return ' '.join(elements)
+
+
 class Intcode:
     """An Elve Intcode computer."""
 
-    def __init__(self, *instructions):
+    def __init__(self, *instructions, trace_execution=False):
         self.program = instructions
         self.memory = None
         self.ip = 0
+        self.trace = None
+        self.trace_execution=trace_execution
 
     @classmethod
     def from_file(cls, path) -> 'Intcode':
-        _logger.debug(f'Loading program from file "{path}".')
         return cls(*[int(c) for c in pathlib.Path(path).read_text().split(',')])
 
     def next_instruction(self):
         instruction = self.memory[self.ip]
-        _logger.debug(f'Read instruction code {instruction} at {self.ip}.')
 
         self.ip += 1
         return instruction
@@ -49,68 +68,78 @@ class Intcode:
         return self.run(inputs=args)
 
     def run(self, inputs: t.Iterable[int] = None) -> t.List[int]:
-        _logger.info('Starting program execution.')
         self.memory = list(self.program)
         self.ip = 0
-        outputs = []
 
         input_iter = iter(inputs) if inputs else None
+        outputs = []
 
-        while True:
+        opcode = None
+        while opcode is not Opcode.EXIT:
+            self.trace = TraceItem(self.ip)
+
             instruction = self.next_instruction()
+            self.trace.instruction = instruction
+
             opcode = Opcode(instruction % 100)
             parameter_modes = instruction // 100
 
             if opcode is Opcode.EXIT:
-                _logger.info('Exiting normally.')
-                break
+                self.trace.mnemonic = 'EXT'
 
-            if opcode is Opcode.ADD:
+            elif opcode is Opcode.ADD:
+                self.trace.mnemonic = 'ADD'
                 self._add(parameter_modes)
 
             elif opcode is Opcode.MULTIPLY:
+                self.trace.mnemonic = 'MUL'
                 self._multiply(parameter_modes)
 
             elif opcode is Opcode.INPUT:
+                self.trace.mnemonic = 'INP'
                 value = next(input_iter)
-                _logger.info(f'Passing input {value}.')
                 self._store(value)
 
             elif opcode is Opcode.OUTPUT:
+                self.trace.mnemonic = 'OUT'
                 value = self._load(ParameterMode(parameter_modes))
-                _logger.info(f'Output {value}.')
                 outputs.append(value)
 
             elif opcode is Opcode.JUMP_IF_TRUE:
+                self.trace.mnemonic = 'JNZ'
                 self._jump_if(True, parameter_modes)
 
             elif opcode is Opcode.JUMP_IF_FALSE:
+                self.trace.mnemonic = 'JZR'
                 self._jump_if(False, parameter_modes)
 
             elif opcode is Opcode.LESS_THAN:
+                self.trace.mnemonic = 'LSS'
                 self._less_than(parameter_modes)
 
             elif opcode is Opcode.EQUALS:
+                self.trace.mnemonic = 'EQU'
                 self._equals(parameter_modes)
 
             else:
                 raise NotImplementedError('unexpected opcode', opcode)
 
+            if self.trace_execution:
+                _logger.debug(self.trace)
+
         return outputs
 
     def _add(self, parameter_modes):
         param1, param2 = self._load_multiple(2, parameter_modes)
-        _logger.info(f'Adding {param1} and {param2}.')
         self._store(param1 + param2)
 
     def _multiply(self, parameter_modes):
         param1, param2 = self._load_multiple(2, parameter_modes)
-        _logger.info(f'Multiplying {param1} and {param2}.')
         self._store(param1 * param2)
 
     def _store(self, value: int):
         address = self.next_instruction()
-        _logger.info(f'Storing {value} at {address}.')
+        self.trace.result = f'({address})'
         self.memory[address] = value
 
     def _load_multiple(self, number: int, parameter_modes: int) -> t.List[int]:
@@ -128,9 +157,11 @@ class Intcode:
         value = self.next_instruction()
 
         if mode is ParameterMode.IMMEDIATE:
+            self.trace.arguments.append(str(value))
             return value
 
         if mode is ParameterMode.POSITION:
+            self.trace.arguments.append(f'({value})')
             return self.memory[value]
 
         raise NotImplementedError('unknown parameter mode', mode)
@@ -138,10 +169,7 @@ class Intcode:
     def _jump_if(self, condition: bool, parameter_modes: int):
         value, address = self._load_multiple(2, parameter_modes)
         if bool(value) is condition:
-            _logger.info(f'Jumping to {address} because {value} is {condition}.')
             self.ip = address
-        else:
-            _logger.info(f'Not jumping to {address} because {value} is {condition}.')
 
     def _less_than(self, parameter_modes):
         param1, param2 = self._load_multiple(2, parameter_modes)
--- solution_05.py
+++ solution_05.py
@@ -1,9 +1,18 @@
+import logging
+
+import colorama
+import daiquiri
+
 from intcode import Intcode
 
 
 def main():
+    colorama.init()
+    daiquiri.setup('DEBUG')
+
     diagnostic = Intcode.from_file('input_05.txt')
     print('air conditioner:', diagnostic(1))
+    diagnostic.trace_execution = True
     print('thermal radiator controller:', diagnostic(5))
 
 
