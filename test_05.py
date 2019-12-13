ca659374dcb1ec72ddadc8a1e554dafbb9bcfdf6
Mike Pagel <mike@mpagel.de>
2019-12-13 20:38:19 +0100

    big test

--- test_05.py
+++ test_05.py
@@ -71,3 +71,15 @@ def test__jump__immediate():
     assert jump(0) == [0]
     assert jump(1) == [1]
 
+
+def test__big_compare_8():
+    equal_8 = Intcode(3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                      1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                      999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99)
+    assert equal_8(-200) == [999]
+    assert equal_8(6) == [999]
+    assert equal_8(7) == [999]
+    assert equal_8(8) == [1000]
+    assert equal_8(9) == [1001]
+    assert equal_8(10) == [1001]
+    assert equal_8(10000) == [1001]
