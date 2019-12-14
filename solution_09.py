1e7e27af59951f70a4988c67dfc9f5263fbba3a6
Mike Pagel <mike@mpagel.de>
2019-12-14 12:24:26 +0100

    part 2

--- solution_09.py
+++ solution_09.py
@@ -4,7 +4,8 @@ from intcode import Intcode
 def main():
     boost = Intcode.from_file('input_09.txt')
     print('BOOST keycode:', boost(1))
-    boost.print_trace()
+    print('BOOST coordinates of the distress signal:', boost(2))
+    # boost.print_trace()
 
 
 if __name__ == '__main__':
