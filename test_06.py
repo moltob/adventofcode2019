d35d1461539c64a5207805ac5d12aeaa89940746
Mike Pagel <mike@mpagel.de>
2019-12-14 11:01:52 +0100

    tests

--- test_06.py
+++ test_06.py
@@ -4,3 +4,14 @@ from intcode import Intcode
 def test__copy():
     program = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
     assert Intcode(*program)() == program
+
+
+def test__16_digits_output():
+    outputs = Intcode(1102, 34915192, 34915192, 7, 4, 7, 99, 0)()
+    assert len(outputs) == 1
+    assert len(str(outputs[0])) == 16
+
+
+def test__large_output():
+    outputs = Intcode(104, 1125899906842624, 99)()
+    assert outputs == [1125899906842624]
