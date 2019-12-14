faefd067b75cfb37bab792f8d9ec7e3e09c8d251
Mike Pagel <mike@mpagel.de>
2019-12-14 10:58:33 +0100

    switched to associate memory

--- intcode.py
+++ intcode.py
@@ -1,3 +1,4 @@
+import collections
 import enum
 import pathlib
 import typing as t
@@ -52,7 +53,7 @@ class Intcode:
 
     def __init__(self, *instructions):
         self.program = instructions
-        self.memory = None
+        self._memory = collections.defaultdict(int)
         self.ip = 0
         self.relative_base = 0
         self.trace = []
@@ -62,6 +63,14 @@ class Intcode:
     def from_file(cls, path) -> 'Intcode':
         return cls(*[int(c) for c in pathlib.Path(path).read_text().split(',')])
 
+    @property
+    def memory(self) -> t.List[int]:
+        if not self._memory:
+            return []
+
+        max_index = max(self._memory.keys())
+        return [self._memory[i] for i in range(max_index + 1)]
+
     def print_trace(self):
         print('======================================')
         print('ADDR  INSTR  COMMAND')
@@ -70,7 +79,7 @@ class Intcode:
         print('======================================')
 
     def next_instruction(self):
-        instruction = self.memory[self.ip]
+        instruction = self._memory[self.ip]
 
         self.ip += 1
         return instruction
@@ -79,7 +88,9 @@ class Intcode:
         return self.run(inputs=args)
 
     def run(self, inputs: t.Iterable[int] = None) -> t.List[int]:
-        self.memory = list(self.program)
+        self._memory.clear()
+        self._memory.update({i: v for i, v in enumerate(self.program)})
+
         self.ip = 0
         self.relative_base = 0
         self.trace = []
@@ -98,51 +109,58 @@ class Intcode:
             self._trace_line.opcode = opcode
             self._trace_line.parameter_mode = parameter_modes
 
-            if opcode is Opcode.EXIT:
-                self._trace_line.mnemonic = 'EXT'
+            try:
 
-            elif opcode is Opcode.ADD:
-                self._trace_line.mnemonic = 'ADD'
-                self._add(parameter_modes)
+                if opcode is Opcode.EXIT:
+                    self._trace_line.mnemonic = 'EXT'
 
-            elif opcode is Opcode.MULTIPLY:
-                self._trace_line.mnemonic = 'MUL'
-                self._multiply(parameter_modes)
+                elif opcode is Opcode.ADD:
+                    self._trace_line.mnemonic = 'ADD'
+                    self._add(parameter_modes)
 
-            elif opcode is Opcode.INPUT:
-                self._trace_line.mnemonic = 'INP'
-                value = next(input_iter)
-                self._store(value)
+                elif opcode is Opcode.MULTIPLY:
+                    self._trace_line.mnemonic = 'MUL'
+                    self._multiply(parameter_modes)
 
-            elif opcode is Opcode.OUTPUT:
-                self._trace_line.mnemonic = 'OUT'
-                value = self._load(ParameterMode(parameter_modes))
-                outputs.append(value)
+                elif opcode is Opcode.INPUT:
+                    self._trace_line.mnemonic = 'INP'
+                    value = next(input_iter)
+                    self._store(value)
 
-            elif opcode is Opcode.JUMP_IF_TRUE:
-                self._trace_line.mnemonic = 'JNZ'
-                self._jump_if(True, parameter_modes)
+                elif opcode is Opcode.OUTPUT:
+                    self._trace_line.mnemonic = 'OUT'
+                    value = self._load(ParameterMode(parameter_modes))
+                    outputs.append(value)
 
-            elif opcode is Opcode.JUMP_IF_FALSE:
-                self._trace_line.mnemonic = 'JZR'
-                self._jump_if(False, parameter_modes)
+                elif opcode is Opcode.JUMP_IF_TRUE:
+                    self._trace_line.mnemonic = 'JNZ'
+                    self._jump_if(True, parameter_modes)
 
-            elif opcode is Opcode.LESS_THAN:
-                self._trace_line.mnemonic = 'LSS'
-                self._less_than(parameter_modes)
+                elif opcode is Opcode.JUMP_IF_FALSE:
+                    self._trace_line.mnemonic = 'JZR'
+                    self._jump_if(False, parameter_modes)
 
-            elif opcode is Opcode.EQUALS:
-                self._trace_line.mnemonic = 'EQU'
-                self._equals(parameter_modes)
+                elif opcode is Opcode.LESS_THAN:
+                    self._trace_line.mnemonic = 'LSS'
+                    self._less_than(parameter_modes)
 
-            elif opcode is Opcode.ADJUST_RELATIVE_BASE:
-                self._trace_line.mnemonic = 'ARB'
-                self._adjust_relative_base(parameter_modes)
+                elif opcode is Opcode.EQUALS:
+                    self._trace_line.mnemonic = 'EQU'
+                    self._equals(parameter_modes)
 
-            else:
-                raise NotImplementedError('unexpected opcode', opcode)
+                elif opcode is Opcode.ADJUST_RELATIVE_BASE:
+                    self._trace_line.mnemonic = 'ARB'
+                    self._adjust_relative_base(parameter_modes)
 
-            self.trace.append(str(self._trace_line))
+                else:
+                    raise NotImplementedError('unexpected opcode', opcode)
+
+                self.trace.append(str(self._trace_line))
+            except:
+                print(f'Program execution failed:')
+                self.print_trace()
+                print(f'{self._trace_line} <-- FAILED')
+                raise
 
         return outputs
 
@@ -157,7 +175,7 @@ class Intcode:
     def _store(self, value: int):
         address = self.next_instruction()
         self._trace_line.result = f'({address})'
-        self.memory[address] = value
+        self._memory[address] = value
 
     def _load_multiple(self, number: int, parameter_modes: int) -> t.List[int]:
         values = []
@@ -179,11 +197,11 @@ class Intcode:
 
         if mode is ParameterMode.POSITION:
             self._trace_line.arguments.append(f'({value})')
-            return self.memory[value]
+            return self._memory[value]
 
         if mode is ParameterMode.RELATIVE:
             self._trace_line.arguments.append(f'/{value}/')
-            return self.memory[self.relative_base + value]
+            return self._memory[self.relative_base + value]
 
         raise NotImplementedError('unknown parameter mode', mode)
 
---
+++ test_06.py
@@ -0,0 +1,6 @@
+from intcode import Intcode
+
+
+def test__copy():
+    program = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+    assert Intcode(*program)() == program
